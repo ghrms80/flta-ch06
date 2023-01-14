fb328ab6ed8d7b06f502a5fc1ab4b0be0e29a944
jiho <ghrms80@gmail.com>
2023-01-14 17:49:07 +0900

    Consuming the changes

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -8,8 +8,18 @@ class GroceryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add a scaffold widget
-    return const EmptyGroceryScreen();
+    // 5
+    return Scaffold(
+      // 6
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          // TODO: Present GroceryItemScreen
+        },
+      ),
+      // 7
+      body: buildGroceryScreen(),
+    );
   }
 
   Widget buildGroceryScreen() {
