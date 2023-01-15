39d127f3fe7298073ce5eb9f310ab1d827cc239e
jiho <ghrms80@gmail.com>
2023-01-15 11:46:26 +0900

    Displaying the importance label / selected date / checkbox

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import '../models/models.dart';
 
@@ -27,7 +29,50 @@ class GroceryTile extends StatelessWidget {
       color: Colors.red,
     );
   }
-  // TODO: Add BuildImportance()
-  // TODO: Add buildDate()
-  // TODO: Add buildCheckbox()
+
+  Widget buildImportance() {
+    if (item.importance == Importance.low) {
+      return Text(
+        'Low',
+        style: GoogleFonts.lato(
+          decoration: textDecoration,
+        ),
+      );
+    } else if (item.importance == Importance.medium) {
+      return Text(
+        'Medium',
+        style: GoogleFonts.lato(
+          fontWeight: FontWeight.w800,
+          decoration: textDecoration,
+        ),
+      );
+    } else if (item.importance == Importance.high) {
+      return Text(
+        'High',
+        style: GoogleFonts.lato(
+          color: Colors.red,
+          fontWeight: FontWeight.w900,
+          decoration: textDecoration,
+        ),
+      );
+    } else {
+      throw Exception('This importance type does not exist');
+    }
+  }
+
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
+
+  Widget buildCheckbox() {
+    return Checkbox(
+      value: item.isComplete,
+      onChanged: onComplete,
+    );
+  }
 }
